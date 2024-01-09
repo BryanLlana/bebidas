ab25a19af111bd4332ec5cf3a945a25990e7b4d8
Bryan Joseph <bryanllana14@gmail.com>
2024-01-09 18:03:24 -0500

    Paginas inicio y favoritos preparadas

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import { RouterLink, useRoute } from 'vue-router'
+  import { computed } from 'vue'
+  
+  const route = useRoute()
+  const paginaInicio = computed(() => {
+    return route.name === 'inicio'
+  })
+
+</script>
+
+<template>
+  <header class="bg-slate-800" :class="{ header: paginaInicio }">
+    <div class="mx-auto container px-5 py-16">
+      <div class="flex justify-between items-center">
+        <div>
+          <RouterLink to="/">
+            <img class="w-32" src="/img/logo.svg" alt="Logotipo" />
+          </RouterLink>
+        </div>
+        <nav class="flex gap-4">
+          <RouterLink :to="{ name: 'inicio' }" class="text-white uppercase font-bold">
+            Inicio
+          </RouterLink>
+          <RouterLink :to="{ name: 'favoritos' }" class="text-white uppercase font-bold">
+            Favoritos
+          </RouterLink>
+        </nav>
+      </div>
+      <form v-if="paginaInicio" class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+        <div class="space-y-4">
+          <label for="ingrediente" class="block text-white uppercase font-extrabold text-lg">
+            Nombre o Ingredientes
+          </label>
+          <input id="ingrediente" type="text" class="p-3 w-full rounded-lg focus:outline-none" placeholder="Ej: Vodka, Tequila, etc">
+        </div>
+        <div class="space-y-4">
+          <label for="categoria" class="block text-white uppercase font-extrabold text-lg">
+            Categoría
+          </label>
+          <select id="categoria" class="p-3 w-full rounded-lg focus:outline-none">
+            <option value="">--Seleccione--</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Buscar Recetas" class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase">
+      </form>
+    </div>
+  </header>
+</template>
+
+<style>
+  .header {
+    background-image: url('/img/bg.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+</style>
